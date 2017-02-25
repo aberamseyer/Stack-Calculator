73cf53bd4a5ca905b555bc7856fc0960fbb24cd5
Ramseyer, Abe <aramsey@ilstu.edu>
2017-02-25 14:50:31 -0600

    finished merge with stack work

--- Stack.cpp
+++ Stack.cpp
@@ -3,6 +3,30 @@
  * Implemenation of the Stack ADT using a LinkedList as a private
  * member.
  *
- * @author
+ * @author Griffin
  *
  */
+#include "Stack.h"
+#include "LinkedList.h"
+
+LinkedList stack;
+
+Stack::Stack() {
+	s = stack;
+};
+
+void Stack::push(std::string &x) {
+	s.add(x);
+};
+
+void Stack::pop() {
+	int a = stack.getSize() - 1;
+	std::string b = stack.at(a);
+	s.remove(b);
+};
+
+std::string& Stack::top() {
+	return s.at(stack.getSize() - 1);
+};
+
+
--- Stack.h
+++ Stack.h
@@ -1,6 +1,24 @@
+
 /*
 * Header file for the Stack implementation
 *
-* @author
+* @author Griffin
 *
 */
+#ifndef STACK_H
+#define STACK_H
+
+#include <list>
+
+class Stack{
+  public:
+    Stack ();
+    void push (std::string & x);
+    void pop ();
+    std::string & top ();
+    // add more functions as needed
+  private:
+    LinkedList s;
+};
+
+#endif
